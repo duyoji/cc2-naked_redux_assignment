b05c05b0a4bdcba5e972def0b107acdb9b9fe190
Tsuyoshi Maeda <oasis5tsuyoshi@gmail.com>
2017-11-11 23:54:46 +0900

    Added code for redux extention.

--- src/stores/index.js
+++ src/stores/index.js
@@ -8,7 +8,10 @@ const getInstance = () => {
     return store;
   }
 
-  store = createStore(reducer);
+  store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
   return store;
 };
 
